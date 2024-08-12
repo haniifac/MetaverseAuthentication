423b54593dd83d2e9dc8005f4ac211ebff0230c4
synERR@3am <haniifcandra@gmail.com>
2024-08-12 15:30:09 +0700

    initial commit

---
+++ src/main/java/com/virtualclassroom/authrestapi/AuthrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.virtualclassroom.authrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthrestapiApplication.class, args);
+	}
+
+}
